04b965e2a3ebbf4c4d4b1ef4a476656118bfc464
juliankailich <julian.kailich@udarealestate.com>
2024-07-21 20:58:35 -0300

    Fix render

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from "react";
 import {
-  createBrowserRouter,
-  RouterProvider
+  BrowserRouter as Router,
+  Routes,
+  Route
 } from "react-router-dom";
-import { createRoot } from "react-dom/client";
 import { WelcomeInput } from "./components/welcomeInput";
 import { Home } from "./pages/home";
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <WelcomeInput />,
-    },
-    {
-      path: "/home",
-      element: <Home />,
-    },
-  ]);
-
-  createRoot(document.getElementById("root")).render(
-    <RouterProvider router={router} />
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<WelcomeInput />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
