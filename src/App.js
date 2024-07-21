e259e44c01cfcd7a99008a89f7f9ab6eba0dcd8d
juliankailich <julian.kailich@udarealestate.com>
2024-07-21 18:12:16 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import { WelcomeInput } from "./components/welcomeInput";
+import { Home } from "./pages/home";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <WelcomeInput />,
+    },
+    {
+      path: "/home",
+      element: <Home />,
+    },
+  ]);
+
+  createRoot(document.getElementById("root")).render(
+    <RouterProvider router={router} />
   );
 }
 
